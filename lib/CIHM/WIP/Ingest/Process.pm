be2d60645b637e32c260a2de03211a467c7e27a0
Russell McOrmond <russell.mcormond@canadiana.ca>
2017-11-27 19:30:38 +0000

    Moving Ingest to CIHM::WIP

---
+++ lib/CIHM/WIP/Ingest/Process.pm
@@ -0,0 +1,416 @@
+package CIHM::Meta::Ingest::Process;
+
+use 5.014;
+use strict;
+use CIHM::TDR::TDRConfig;
+use CIHM::TDR::Repository;
+use CIHM::TDR::REST::wipmeta;
+use CIHM::TDR::REST::tdrepo;
+use Try::Tiny;
+use File::Spec;
+use File::Path qw(make_path remove_tree);
+use File::Copy;
+use JSON;
+use Archive::BagIt::Fast;
+use Switch;
+use POSIX qw(strftime);
+
+
+use Data::Dumper;
+
+=head1 NAME
+
+CIHM::Meta::Ingest::Process - Handles the processing of individual AIPs for CIHM::Meta::Ingest
+
+=head1 SYNOPSIS
+
+    my $t_repo = CIHM::TDR::Ingest::Process->new($args);
+      where $args is a hash of arguments.
+
+      $args->{configpath} is as defined in CIHM::TDR::TDRConfig
+
+=cut
+
+
+sub new {
+    my($class, $args) = @_;
+    my $self = bless {}, $class;
+
+    if (ref($args) ne "HASH") {
+        die "Argument to CIHM::TDR::Replication->new() not a hash\n";
+    };
+    $self->{args} = $args;
+
+    if (!$self->config) {
+        die "TDRConfig object parameter is mandatory\n";
+    }
+    if (!$self->tdr) {
+        die "CIHM::TDR instance parameter is mandatory\n";
+    }
+    if (!$self->wipmeta) {
+        die "wipmeta object parameter is mandatory\n";
+    }
+    if (!$self->cserver) {
+        die "cserver object parameter is mandatory\n";
+    }
+    if (!$self->repo) {
+        die "repo object parameter is mandatory\n";
+    }
+    if (!$self->aip) {
+        die "Parameter 'aip' is mandatory\n";
+    }
+    if (!$self->tempdir) {
+        die "Parameter 'tempdir' is mandatory\n";
+    }
+    if (!$self->outbox) {
+        die "Parameter 'outbox' is mandatory\n";
+    }
+    if (!$self->stages) {
+        die "Parameter 'stages' is mandatory\n";
+    }
+
+    $self->{aipdata}=$self->wipmeta->get_aip($self->aip);
+    if (!$self->aipdata) {
+        die "Failed retrieving AIP data\n";
+    }
+    return $self;
+}
+
+# Simple accessors for now -- Do I want to Moo?
+sub args {
+    my $self = shift;
+    return $self->{args};
+}
+sub aip {
+    my $self = shift;
+    return $self->args->{aip};
+}
+sub aipdata {
+    my $self = shift;
+    return $self->{aipdata};
+}
+sub ingestReq {
+    my $self = shift;
+    return $self->aipdata->{'processReq'}[0];
+}
+sub ingesttype {
+    my $self = shift;
+    return $self->ingestReq->{'type'}
+}
+sub aiprepos {
+    my $self = shift;
+    return $self->{aiprepos};
+}
+sub configpath {
+    my $self = shift;
+    return $self->args->{configpath};
+}
+sub config {
+    my $self = shift;
+    return $self->args->{config};
+}
+sub log {
+    my $self = shift;
+    return $self->config->logger;
+}
+sub wipmeta {
+    my $self = shift;
+    return $self->args->{wipmeta};
+}
+sub tdr {
+    my $self = shift;
+    return $self->args->{tdr};
+}
+sub cserver {
+    my $self = shift;
+    return $self->args->{cserver};
+}
+sub repo {
+    my $self = shift;
+    return $self->args->{repo};
+}
+sub tdrepo {
+    my $self = shift;
+    return $self->repo->tdrepo;
+}
+sub hostname {
+    my $self = shift;
+    return $self->args->{hostname};
+}
+sub tempdir {
+    my $self = shift;
+    return $self->args->{tempdir};
+}
+sub outbox {
+    my $self = shift;
+    return $self->args->{outbox};
+}
+sub stages {
+    my $self = shift;
+    return $self->args->{stages};
+}
+sub aipdir {
+    my $self = shift;
+    return File::Spec->catfile($self->tempdir,$self->aip);
+}
+
+sub process {
+    my ($self) = @_;
+
+    $self->log->info($self->aip.": Accepted job. ingestReq = ". encode_json($self->ingestReq));
+
+    my $aipdir = $self->aipdir;
+    $self->ingest_setup($aipdir);
+
+
+    switch ($self->ingesttype) {
+
+        case "new"  {
+            make_path("$aipdir/data/revisions") or die("Failed to create $aipdir/data/revisions: $!");
+            $self->copy_sip($aipdir);
+            $self->tdr->changelog($aipdir, "Created new AIP");
+            $self->log->info($self->aip.": Created new AIP in $aipdir");
+        }
+
+        case "update" {
+            my $revision_name = strftime("%Y%m%dT%H%M%S", gmtime(time));
+            move("$aipdir/data/sip", "$aipdir/data/revisions/$revision_name") or
+                die("Failed to move $aipdir/data/sip to $aipdir/data/revisions/$revision_name: $!");
+            $self->copy_sip($aipdir);
+
+            # Check for duplicate metadata.xml 
+            $self->checkdup($aipdir);
+
+            $self->tdr->changelog($aipdir, "Updated SIP; old SIP stored as revision $revision_name");
+            $self->log->info($self->aip.": Updated SIP in $aipdir; old SIP stored as revision $revision_name");
+        }
+
+        case "metadata" {
+            my $revision_name = strftime("%Y%m%dT%H%M%S.partial", gmtime(time));
+            mkdir("$aipdir/data/revisions/$revision_name") or
+                die("Failed to create $aipdir/revisions/$revision_name: $!");
+            move("$aipdir/data/sip/data/metadata.xml", "$aipdir/data/revisions/$revision_name/metadata.xml") or
+                die("Failed tp move $aipdir/data/sip/data/metadata.xml to $aipdir/data/revisions/$revision_name/metadata.xml: $!");
+            copy("$aipdir/data/sip/manifest-md5.txt",  "$aipdir/data/revisions/$revision_name/manifest-md5.txt") or
+                die("Failed to copy $aipdir/data/sip/manifest-md5.txt to $aipdir/data/revisions/$revision_name/manifest-md5.txt: $!");
+
+            # Get metadata attachment
+            my $res = $self->wipmeta->get("/".$self->wipmeta->database."/".$self->aip."/metadata.xml");
+            if ($res->code == 200) {
+                open(my $fh, '>', "$aipdir/data/sip/data/metadata.xml");
+                # Store content without deserialization
+                print $fh $res->response->content;
+                close $fh;
+            }
+            else {
+                die "Get of metadata.xml return code: ".$res->code."\n"; 
+            }
+
+            # Check for duplicate metadata.xml 
+            $self->checkdup($aipdir);
+
+            # Update the SIP bagit info
+            CIHM::Bagit::write_bagit("$aipdir/data/sip");
+            CIHM::Bagit::manifest_md5("$aipdir/data/sip");
+
+            $self->tdr->changelog($aipdir, "Updated metadata record; old record stored in revision $revision_name");
+            $self->log->info($self->aip.": Updated metadata record in $aipdir; old record stored in revision $revision_name");
+        }
+        else {
+            die "ingest type of ".$self->ingestype." invalid\n";
+        }
+    }
+
+    my $validatetemp=$self->tempdir."/sipvalidate";
+    if (! -d $validatetemp) {
+        make_path($validatetemp) or die("Failed to create $validatetemp: $!");
+    }
+
+    my $sip = CIHM::TDR::SIP->new("$aipdir/data/sip");
+    $sip->validate(1,$validatetemp);
+
+    $self->tdr->changelog($aipdir, $self->ingestReq->{'changelog'});
+    $self->log->info($self->aip.": Changelog: ". $self->ingestReq->{'changelog'});
+
+    # Generate BagIt information files for the AIP
+    $self->tdr->write_bagit($aipdir);
+
+    # Get basic information about AIP
+    my $ingestdoc = $self->repo->get_manifestinfo($aipdir);
+
+    # rsync to incoming of TDR file repository..
+    my $destdir = $self->outbox."/".$self->aip;
+    $self->rsync($aipdir."/.",$destdir);
+
+    $self->log->info($self->aip.": rsync $aipdir $destdir");
+
+    # Remove temporary AIP build directory
+    remove_tree($aipdir) or die("Failed to remove $aipdir: $!");
+
+    $self->log->info($self->aip.": Done processing");
+
+    return $ingestdoc;
+}
+
+
+sub copy_sip {
+    my ($self,$aipdir) = @_;
+
+    make_path("$aipdir/data/sip") or die("Failed to create $aipdir/data/sip: $!");
+
+    my $fromsip;
+    if (exists $self->ingestReq->{'rsyncurl'}) {
+        $fromsip=$self->ingestReq->{'rsyncurl'}."/.";
+    } else {
+        if (!defined $self->aipdata->{filesystem}) {
+            die "Required filesystem field not defined\n";
+        }
+        my $stage = $self->aipdata->{filesystem}->{stage} or
+            die "Filesystem sub-field 'stage' not defined\n";
+        my $stagedir = $self->stages->{$stage} or
+            die "WIP Filesystem stage=$stage not configured\n";
+
+        my $configid = $self->aipdata->{filesystem}->{configid} or
+            die "Filesystem sub-field 'configid' not defined\n";
+        my $identifier = $self->aipdata->{filesystem}->{identifier} or
+            die "Filesystem sub-field 'identifier' not defined\n";
+
+        $fromsip=$stagedir."/".$configid."/".$identifier.
+            "/sip/.";
+    }
+
+    $self->rsync($fromsip,"$aipdir/data/sip/.");
+
+    my $verified;
+    try {
+        my $bagit = new Archive::BagIt::Fast("$aipdir/data/sip");
+        my $valid = $bagit->verify_bag();
+        $verified = $valid;
+    };
+    if (!$verified) {
+        # Bag wasn't valid.
+        die "Error verifying sip copied from: $fromsip\n";
+    }
+}
+
+
+sub ingest_setup {
+    my ($self,$aipdir) = @_;
+
+    my $res = $self->tdrepo->post("/".$self->tdrepo->database."/_design/tdr/_view/newestaip?group=true",{ keys => [ $self->aip ]}, {deserializer => 'application/json'});
+    if ($res->code == 200) {
+        if (defined $res->data->{rows} && scalar(@{$res->data->{rows}})) {
+            $self->{aiprepos} = \@{$res->data->{rows}->[0]->{value}[1]};
+        }
+    } else {
+        die "_view/newestaip during ingest_setup() for ".$self->aip." returned: " . $res->code."\n";
+    }
+
+
+    if ($self->aiprepos) {
+        if ($self->ingesttype eq 'new' ) {
+            die "Type is new, but ".$self->aip." already exists in tdrepo\n";
+        }
+
+        my @rrepos = $self->cserver->replication_repositories();
+        my $copyrepo;
+        # Loop though repos we would be willing to sync from
+      REPOL: foreach my $fromrepo (@rrepos) {
+          foreach my $thisrepo (@{$self->aiprepos}) {
+              if ($thisrepo eq $fromrepo) {
+                  $copyrepo=$thisrepo;
+                  last REPOL;
+              }
+          }
+      }
+        if (!$copyrepo) {
+            die ("Couldn't find repo to copy ".$self->aip."\n")
+        }
+        my $aipinfo=$self->cserver->get_aipinfo($self->aip,$copyrepo);
+        if (!$aipinfo) {
+            die "Couldn't get AIP information from $copyrepo\n";
+        }
+
+        if (defined $aipinfo->{rsyncpath}) {
+            mkdir $aipdir;
+            $self->rsync($aipinfo->{rsyncpath}."/.",$aipdir."/.");
+
+            my $verified;
+            try {
+                my $bagit = new Archive::BagIt::Fast($aipdir);
+                my $valid = $bagit->verify_bag();
+                $verified = $valid;
+            };
+            if (!$verified) {
+                # Bag wasn't valid.
+                die "Error verifying bag: $aipdir\n";
+            }
+        } else {
+            die "rsync path not defined in aipinfo on $copyrepo\n";
+        }
+    } else {
+        if ($self->ingesttype ne 'new' ) {
+            die "Type is ".$self->ingesttype.", but ".$self->aip." doesn't exists in tdrepo\n";
+        }
+        if (-d $aipdir) {
+            remove_tree($aipdir) or die("Failed to remove old AIP attempt at $aipdir: $!");
+        } 
+    }
+}
+
+sub checkdup {
+    my ($self,$aipdir) = @_;
+
+    my $aip = $self->aip;
+
+    my $metadata="$aipdir/data/sip/data/metadata.xml";
+    open(METADATA, "<", $metadata) or die "Can't open $metadata\n";
+    binmode(METADATA);
+    my $metadatamd5=Digest::MD5->new->addfile(*METADATA)->hexdigest;
+    close(METADATA);
+
+    # Check for duplicate metadata MD5
+    open my $fh,"<$aipdir/manifest-md5.txt"
+        or die("Can't open manifest file within $aipdir\n");
+    while (my $line = <$fh>) {
+        chomp($line);
+        if (substr($line,-12) eq 'metadata.xml') {
+            my ($md5,$filename)=split /\s+/, $line;
+            if($md5 eq $metadatamd5) {
+                die("metadata.xml from SIP matches existing AIP: $filename\n");
+            }
+        }
+    }
+    close ($fh);
+}
+
+
+
+sub rsync {
+    my ($self,$source,$destination) = @_;
+
+    # Don't preserve owner or group, so run as intended user.
+    my @rsynccmd=("rsync","-rlpt","--del","--partial","--timeout=10",$source,$destination);
+
+    my $rsyncexit = 30;
+    # https://download.samba.org/pub/rsync/rsync.html
+    # 10 - Error in socket I/O
+    # 12 - Error in rsync protocol data stream
+    # 30 - Timeout in data send/receive
+    while ($rsyncexit == 10 || $rsyncexit == 12 ||  $rsyncexit == 30 ) {
+        system(@rsynccmd);
+        if ($? == -1) {
+            die "@rsynccmd -- failed to execute: $!\n";
+        }
+        elsif ($? & 127) {
+            die "@rsynccmd -- nchild died with signal %d, %s coredump\n",
+            ($? & 127),  ($? & 128) ? 'with' : 'without';
+        }
+        $rsyncexit =  $? >> 8;
+    }
+    if ($rsyncexit) {
+        die "@rsynccmd -- child exited with value $rsyncexit\n";
+    }
+}
+
+1;
---
+++ lib/CIHM/WIP/Ingest/Worker.pm
@@ -0,0 +1,221 @@
+package CIHM::Meta::Ingest::Worker;
+
+use strict;
+use AnyEvent;
+use Try::Tiny;
+use CIHM::TDR;
+use CIHM::TDR::TDRConfig;
+use CIHM::TDR::Repository;
+use CIHM::TDR::REST::wipmeta;
+use CIHM::TDR::REST::tdrepo;
+use CIHM::TDR::ContentServer;
+use CIHM::Meta::Ingest::Process;
+use JSON;
+use Data::Dumper;
+use Net::Domain qw(hostname hostfqdn hostdomain domainname);
+
+our $self;
+
+sub initworker {
+    my $configpath = shift;
+    our $self;
+
+    AE::log debug => "Initworker ($$): $configpath";
+
+    $self = bless {};
+
+    $self->{hostname} = hostfqdn();
+
+    if (! ($self->{tdr} = CIHM::TDR->new($configpath))) {
+        die "Wasn't able to build CIHM::TDR object\n";
+    }
+    if (! ($self->{repo} = $self->{tdr}->repo)) {
+        die "Wasn't able to build Repository object\n";
+    }
+
+    $self->{config} = CIHM::TDR::TDRConfig->instance($configpath);
+    $self->{logger} = $self->{config}->logger;
+
+    if (! $self->repo->tdrepo) {
+        die "Missing <tdrepo> configuration\n";
+    }
+
+    # Confirm there is a named repository block in the config
+    my %confighash = %{$self->{config}->get_conf};
+
+    # So far we only need a few options (existance checked earlier)
+    $self->{tempdir} = $confighash{ingest}{tempdir};
+    $self->{outbox} = $confighash{ingest}{outbox};
+    $self->{outbox} =~ s|/+$||; # trim any trailing slash
+    if (exists $confighash{stages} &&
+        ref($confighash{stages}) eq "HASH") {
+        $self->{stages} = $confighash{stages};
+    } else {
+        die "Missing <stages> configuration\n"
+    }
+
+    # Undefined if no <wipmeta> config block
+    if (exists $confighash{wipmeta}) {
+        $self->{wipmeta} = new CIHM::TDR::REST::wipmeta (
+            server => $confighash{wipmeta}{server},
+            database => $confighash{wipmeta}{database},
+            type   => 'application/json',
+            conf   => $configpath,
+            clientattrs => {timeout => 3600},
+            );
+    } else {
+        die "Missing <wipmeta> configuration block in config\n";
+    }
+
+    $self->{cserver} = new CIHM::TDR::ContentServer($configpath);
+    if (!$self->{cserver}) {
+        die "Missing ContentServer configuration.\n";
+    }
+}
+
+
+# Simple accessors for now -- Do I want to Moo?
+sub log {
+    my $self = shift;
+    return $self->{logger};
+}
+sub config {
+    my $self = shift;
+    return $self->{config};
+}
+sub wipmeta {
+    my $self = shift;
+    return $self->{wipmeta};
+}
+sub tdr {
+    my $self = shift;
+    return $self->{tdr};
+}
+sub cserver {
+    my $self = shift;
+    return $self->{cserver};
+}
+sub repo {
+    my $self = shift;
+    return $self->{repo};
+}
+sub hostname {
+    my $self = shift;
+    return $self->{hostname};
+}
+sub tempdir {
+    my $self = shift;
+    return $self->{tempdir};
+}
+sub outbox {
+    my $self = shift;
+    return $self->{outbox};
+}
+sub stages {
+    my $self = shift;
+    return $self->{stages};
+}
+
+
+sub warnings {
+    my $warning = shift;
+    our $self;
+    my $aip="unknown";
+
+    if ($self) {
+        $self->{message} .= $warning;
+        $aip = $self->{aip};
+    }
+    # Strip wide characters before  trying to log
+    $warning =~ s/[^\x00-\x7f]//g;
+    $self->log->warn($aip.": $warning");
+}
+
+
+sub ingest {
+  my ($aip,$date,$configpath) = @_;
+  our $self;
+
+  # Capture warnings
+  local $SIG{__WARN__} = sub { &warnings };
+
+  if (!$self) {
+      initworker($configpath);
+  }
+  $self->{aip}=$aip;
+  $self->{message}='';
+
+  AE::log debug => "$aip Before ($$)";
+
+  # Accept Job, but also check if someone else already accepted it
+  my $res = $self->wipmeta->update_basic($aip,{
+      "processing" => encode_json({
+          request => 'ingest',
+          reqdate => $date,
+          host => $self->hostname
+                                  })
+                                         });
+  if (!$res) {
+      $self->log->error("no result from attempt to set processhost");
+      return ($aip);
+  } elsif ($res eq 'no processReq') {
+      $self->log->info("$aip: already completed by other host");
+      return ($aip);
+  } elsif ($res eq 'already set') {
+      $self->log->info("$aip: Process already accepted by this host");
+      return ($aip);
+  } elsif ($res eq 'other host') {
+      $self->log->info("$aip: Process accepted by other host");
+      return ($aip);
+  } elsif ($res eq 'update') {
+
+      my $processdoc = {};
+      my $status;
+
+      # Handle and record any errors
+      try {
+          $status = JSON::true;
+          my $process = new  CIHM::Meta::Ingest::Process(
+              {
+                  aip => $aip,
+                  configpath => $configpath,
+                  config => $self->config,
+                  tdr => $self->tdr,
+                  wipmeta => $self->wipmeta,
+                  cserver => $self->cserver,
+                  repo => $self->repo,
+                  hostname => $self->hostname,
+                  tempdir => $self->tempdir,
+                  outbox => $self->outbox,
+                  stages => $self->stages
+              });
+          $processdoc = $process->process;
+      } catch {
+          $status = JSON::false;
+          $self->log->error("$aip: $_");
+          $self->{message} .= "Caught: " . $_;
+      };
+      $processdoc->{status}=$status;
+      $processdoc->{message}=$self->{message};
+      $processdoc->{request}='ingest';
+      $processdoc->{reqdate}=$date;
+      $processdoc->{host}=$self->hostname;
+      $self->postResults($aip,$processdoc);
+  } else {
+      $self->log->error("$aip: acceptJob returned $res");
+  }
+
+  AE::log debug => "$aip After ($$)";
+
+  return ($aip);
+}
+
+sub postResults {
+    my ($self,$aip,$processdoc) = @_;
+
+    $self->wipmeta->update_basic($aip,{ 
+        "processed" => encode_json($processdoc)
+    });
+}
+
+1;
