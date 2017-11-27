5dd5a1179d4b037bd5596e7a5f9d0fd42677f44f
Russell McOrmond <russell.mcormond@canadiana.ca>
2017-11-27 18:44:52 +0000

    Moving image conversion properly to CIHM::WIP

---
+++ lib/CIHM/WIP/Imageconv/Process.pm
@@ -0,0 +1,254 @@
+package CIHM::Meta::Imageconv::Process;
+
+use 5.014;
+use strict;
+use Try::Tiny;
+use File::Spec;
+use File::Path qw(make_path remove_tree);
+use File::Copy;
+use JSON;
+use Switch;
+use POSIX qw(strftime);
+use Image::Magick;
+use Data::Dumper;
+
+=head1 NAME
+
+CIHM::Meta::Imageconv::Process - Handles the processing of individual AIPs for image conversion.
+
+=head1 SYNOPSIS
+
+    my $t_repo = CIHM::Meta::Imageconv::Process->new($args);
+      where $args is a hash of arguments.
+
+      $args->{configpath} is as defined in CIHM::TDR::TDRConfig
+
+=cut
+
+
+sub new {
+    my($class, $args) = @_;
+    my $self = bless {}, $class;
+
+    if (ref($args) ne "HASH") {
+        die "Argument to CIHM::Meta::Imageconv::Process->new() not a hash\n";
+    };
+    $self->{args} = $args;
+
+    if (!$self->aip) {
+        die "Parameter 'aip' is mandatory\n";
+    }
+    if (!$self->WIP) {
+        die "CIHM::WIP instance parameter is mandatory\n";
+    }
+    if (!$self->log) {
+        die "log object parameter is mandatory\n";
+    }
+    if (!$self->hostname) {
+        die "hostname parameter is mandatory\n";
+    }
+    $self->{aipdata}=$self->wipmeta->get_aip($self->aip);
+    if (!$self->aipdata) {
+        die "Failed retrieving AIP data\n";
+    }
+    return $self;
+}
+
+# Simple accessors for now -- Do I want to Moo?
+sub args {
+    my $self = shift;
+    return $self->{args};
+}
+sub aip {
+    my $self = shift;
+    return $self->args->{aip};
+}
+sub aipdata {
+    my $self = shift;
+    return $self->{aipdata};
+}
+sub hostname {
+    my $self = shift;
+    return $self->args->{hostname};
+}
+sub log {
+    my $self = shift;
+    return $self->args->{log};
+}
+sub WIP {
+    my $self = shift;
+    return $self->args->{WIP};
+}
+sub wipmeta {
+    my $self = shift;
+    return $self->WIP->{wipmeta};
+}
+sub configdocs {
+    my $self = shift;
+    return $self->WIP->configdocs;
+}
+sub configid {
+    my $self = shift;
+    return $self->{configid};
+}
+sub myconfig {
+    my $self = shift;
+    return $self->{myconfig};
+}
+sub workdir {
+    my $self = shift;
+    return $self->{workdir};
+}
+sub job {
+    my $self = shift;
+    return $self->{job};
+}
+
+sub process {
+    my ($self) = @_;
+
+    $self->{job} = $self->aipdata->{'processReq'}[0];
+
+    $self->log->info($self->aip.": Accepted job. processReq = ". encode_json($self->job));
+
+    # Set more per-AIP information
+    if (!defined $self->aipdata->{filesystem}) {
+        die "Required filesystem field not defined\n";
+    }
+
+    $self->{configid} = $self->aipdata->{filesystem}->{configid} or
+        die "Filesystem sub-field 'configid' not defined\n";
+    $self->{myconfig} =$self->WIP->configdocs->{$self->configid} or
+        die $self->configid." is not a valid configuration id\n";
+
+    my ($stage,$stagedir,$identifier);
+
+    $stage = $self->aipdata->{filesystem}->{stage} or
+        die "Filesystem sub-field 'stage' not defined\n";
+    $stagedir = $self->WIP->stages->{$stage} or
+        die "Filesystem stage=$stage not configured\n";
+
+    $identifier = $self->aipdata->{filesystem}->{identifier} or
+        die "Filesystem sub-field 'identifier' not defined\n";
+
+    $self->{workdir}=$stagedir."/".$self->configid."/".$identifier;
+
+    if (! -d $self->workdir) {
+        die "Working directory ".$self->workdir." doesn't exist\n";
+    }
+
+    $self->log->info($self->aip.": Workdir=".$self->workdir);
+
+
+    if($self->job->{fileconfig}) {
+        my @convertlist;
+
+        if (opendir (my $dh, $self->workdir)) {
+            while(readdir $dh) {
+                next if $_ eq "." || $_ eq "..";
+                if (-d $self->workdir."/$_") {
+                    next;
+                }
+                my $fileconfig=$self->WIP->find_fileconfig($self->configid,$_);
+                if ($fileconfig) {
+                    next if ($fileconfig->{ignore});
+                    my $magick=$fileconfig->{magick};
+                    my $ext=$fileconfig->{ext};
+                    next if (!$magick || !$ext);
+                    push @convertlist, { file => $_, fileconfig => $fileconfig};
+                }
+            }
+            closedir $dh;
+        } else {
+            die "Couldn't open ".$self->workdir.": $!\n";
+        }
+
+        $self->log->info($self->aip.": ".scalar(@convertlist)." images need conversion.");
+
+        if (@convertlist) {
+            my $imageconv=$self->workdir."/imageconv";
+            if (! -d $imageconv) {
+                mkdir $imageconv or die "Can't make $imageconv : $!";
+            }
+            my $imageorig=$imageconv."/orig";
+            if (! -d $imageorig) {
+                mkdir $imageorig or die "Can't make $imageorig : $!";
+            }
+
+            my @successlist;
+            foreach my $conv (@convertlist) {
+                my $file=$conv->{file};
+                my $fileconfig=$conv->{fileconfig};
+                my $ext = $fileconfig->{ext};
+
+                my $sourcepath=$self->workdir."/".$file;
+                my $origpath=$imageorig."/".$file;
+
+                my $convfile = $file;
+                $convfile =~ s/\.[^.]*$//;
+                $convfile .= ".".$ext;
+                my $convpath=$imageconv."/".$convfile;
+                my $destpath=$self->workdir."/".$convfile;
+
+                if (-e $origpath) {
+                    unlink $origpath or die "Can't remove $origpath: $!\n";
+                }
+                if (!link($sourcepath,$origpath)) {
+                    die "Error creating link from $sourcepath to $origpath: $!\n";
+                }
+                if (-e $convpath) {
+                    unlink $convpath or die "Can't remove $convpath: $!\n";
+                }
+                $self->domagick($origpath,$convpath,$fileconfig->{magick});
+                push @successlist, { 
+                    unlink => $sourcepath, 
+                    sourcelink => $convpath,
+                    destlink => $destpath
+                };
+            }
+
+            # Only if we got this far do we move the converted files into place
+            foreach my $conv (@successlist) {
+                unlink($conv->{unlink}) 
+                    or die "Can't remove ".$conv->{unlink}.": $!\nObject directory may need cleanup!";
+                if (!link($conv->{sourcelink},$conv->{destlink})) { 
+                    die "Can't link ".$conv->{sourcelink}.
+                        " to ".$conv->{destlink}.": $!\nObject directory may need cleanup!";
+                }
+            }
+        }
+    }
+
+    $self->log->info($self->aip.": Completed job.");
+    return;
+}
+
+
+sub domagick {
+    my ($self,$origpath,$convpath,$magick) = @_;
+
+    my $p = new Image::Magick;
+
+    my $status = $p->Read($origpath);
+
+    if ($status) {
+        switch ($status) {
+            # Skip Exif ImageUniqueID
+            case /Unknown field with tag 42016 / {}
+            else {
+                die "$origpath Read: $status\n";
+            }
+        }
+    }
+
+    foreach my $t (@{$magick}) {
+        if (defined $t->{set}) {
+            $status = $p->Set(%{$t->{set}});
+            die "$origpath Set: $status\n" if "$status";
+        }
+    }
+    $status = $p->Write($convpath);
+    die "$convpath Write: $status\n" if "$status";
+}
+
+1;
---
+++ lib/CIHM/WIP/Imageconv/Worker.pm
@@ -0,0 +1,142 @@
+package CIHM::Meta::Imageconv::Worker;
+
+use strict;
+use AnyEvent;
+use Try::Tiny;
+use CIHM::WIP;
+use CIHM::Meta::Imageconv::Process;
+use CIHM::WIP::REST::ContentServer;
+use JSON;
+use Data::Dumper;
+use Log::Log4perl;
+use Net::Domain qw(hostname hostfqdn hostdomain domainname);
+
+our $self;
+
+sub initworker {
+    my $configpath = shift;
+    our $self;
+
+    AE::log debug => "Initworker ($$): $configpath";
+
+    $self = bless {};
+
+    $self->{hostname} = hostfqdn();
+
+    if (! ($self->{WIP} = CIHM::WIP->new($configpath))) {
+        die "Wasn't able to build CIHM::WIP object\n";
+    }
+
+    Log::Log4perl->init_once("/etc/canadiana/wip/log4perl.conf");
+    $self->{logger} = Log::Log4perl::get_logger("CIHM::Meta::Imageconv");
+
+}
+
+
+# Simple accessors for now -- Do I want to Moo?
+sub log {
+    my $self = shift;
+    return $self->{logger};
+}
+sub WIP {
+    my $self = shift;
+    return $self->{WIP};
+}
+sub wipmeta {
+    my $self = shift;
+    return $self->WIP->wipmeta;
+}
+sub hostname {
+    my $self = shift;
+    return $self->{hostname};
+}
+
+
+sub warnings {
+    my $warning = shift;
+    our $self;
+    my $aip="unknown";
+
+    if ($self) {
+        $self->{message} .= $warning;
+        $aip = $self->{aip};
+    }
+    # Strip wide characters before  trying to log
+    $warning =~ s/[^\x00-\x7f]//g;
+    $self->log->warn($aip.": $warning");
+}
+
+
+sub imageconv {
+  my ($aip,$date,$configpath) = @_;
+  our $self;
+
+  # Capture warnings
+  local $SIG{__WARN__} = sub { &warnings };
+
+  if (!$self) {
+      initworker($configpath);
+  }
+  $self->{aip}=$aip;
+  $self->{message}='';
+
+  AE::log debug => "$aip Before ($$)";
+
+  # Accept Job
+  my $res = $self->wipmeta->update_basic($aip,{ 
+      "processing" => encode_json({
+          request => 'imageconv',
+          reqdate => $date,
+          host => $self->hostname
+                                  })
+                                         });
+  if (!$res) {
+      $self->log->error("no result from attempt to set to imageconv");
+      return ($aip);
+  } elsif ($res eq 'no processReq') {
+      $self->log->info("$aip: no imageconv request -- two instances running?");
+      return ($aip);
+  } elsif ($res ne 'update') {
+      $self->log->error("$aip: acceptJob returned $res");
+      return ($aip);
+  }
+
+  my $status;
+  # Handle and record any errors
+  try {
+      $status = JSON::true;
+      new  CIHM::Meta::Imageconv::Process(
+          {
+              aip => $aip,
+              configpath => $configpath,
+              WIP => $self->WIP,
+              log => $self->log,
+              hostname => $self->hostname
+          })->process;
+  } catch {
+      $status = JSON::false;
+      $self->log->error("$aip: $_");
+      $self->{message} .= "Caught: " . $_;
+  };
+  my $processdoc = {};
+  $processdoc->{status}=$status;
+  $processdoc->{message}=$self->{message};
+  $processdoc->{request}='imageconv';
+  $processdoc->{reqdate}=$date;
+  $processdoc->{host}=$self->hostname;
+  $self->postResults($aip,$processdoc);
+
+  AE::log debug => "$aip After ($$)";
+
+  return ($aip);
+}
+
+sub postResults {
+    my ($self,$aip,$processdoc) = @_;
+
+    $self->wipmeta->update_basic($aip,{ 
+        "processed" => encode_json($processdoc)
+    });
+}
+
+1;
