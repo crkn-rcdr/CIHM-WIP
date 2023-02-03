3e72f46843b7286e0de6040a2e346fadbb7e4355
Russell McOrmond <rmcormond@crkn.ca>
2023-02-03 08:51:56 -0500

    Clean up old bag files before make_bag() (#11)

--- lib/CIHM/WIP/REST/wipmeta.pm
+++ lib/CIHM/WIP/REST/wipmeta.pm
@@ -26,9 +26,9 @@ sub BUILD {
     my $self = shift;
     my $args = shift;
 
-    $self->{conf} = $args->{conf}; 
+    $self->{conf}     = $args->{conf};
     $self->{database} = $args->{database};
-    $self->set_persistent_header('Accept' => 'application/json');
+    $self->set_persistent_header( 'Accept' => 'application/json' );
 }
 
 # Simple accessors for now -- Do I want to Moo?
@@ -54,35 +54,38 @@ sub database {
 =cut
 
 sub update_basic {
-  my ($self, $uid, $updatedoc) = @_;
-  my ($res, $code, $data);
+    my ( $self, $uid, $updatedoc ) = @_;
+    my ( $res, $code, $data );
 
-  # This encoding makes $updatedoc variables available as form data
-  $self->type("application/x-www-form-urlencoded");
-  $res = $self->post("/".$self->database."/_design/tdr/_update/basic/".$uid, $updatedoc, {deserializer => 'application/json'});
+    $self->type("application/json");
+    $res =
+      $self->post( "/" . $self->database . "/_design/tdr/_update/basic/" . $uid,
+        $updatedoc, { deserializer => 'application/json' } );
 
-  if ($res->code != 201 && $res->code != 200) {
-      warn "_update/basic/$uid POST return code: " . $res->code . "\n";
-  }
-  if ($res->data) {
-      return $res->data->{return};
-  }
+    if ( $res->code != 201 && $res->code != 200 ) {
+        warn "_update/basic/$uid POST return code: " . $res->code . "\n";
+    }
+    if ( $res->data ) {
+        return $res->data->{return};
+    }
 }
 
 sub update_filesystem {
-  my ($self, $uid, $updatedoc) = @_;
-  my ($res, $code, $data);
+    my ( $self, $uid, $updatedoc ) = @_;
+    my ( $res, $code, $data );
 
-  # This encoding makes $updatedoc variables available as form data
-  $self->type("application/x-www-form-urlencoded");
-  $res = $self->post("/".$self->database."/_design/tdr/_update/filesystem/".$uid, $updatedoc, {deserializer => 'application/json'});
+    # This encoding makes $updatedoc variables available as form data
+    $self->type("application/x-www-form-urlencoded");
+    $res = $self->post(
+        "/" . $self->database . "/_design/tdr/_update/filesystem/" . $uid,
+        $updatedoc, { deserializer => 'application/json' } );
 
-  if ($res->code != 201 && $res->code != 200) {
-      warn "_update/filesystem/$uid POST return code: " . $res->code . "\n";
-  }
-  if ($res->data) {
-      return $res->data;
-  }
+    if ( $res->code != 201 && $res->code != 200 ) {
+        warn "_update/filesystem/$uid POST return code: " . $res->code . "\n";
+    }
+    if ( $res->data ) {
+        return $res->data;
+    }
 }
 
 =head2 put_attachment
@@ -101,108 +104,128 @@ sub update_filesystem {
 =cut
 
 sub put_attachment {
-  my ($self, $uid, $args) = @_;
-  my ($res, $revision, $updatedoc);
+    my ( $self, $uid, $args ) = @_;
+    my ( $res, $revision, $updatedoc );
 
-  if(exists $args->{updatedoc}) {
-      $updatedoc = $args->{updatedoc};
-  } else {
-      $updatedoc = {};
-  }
+    if ( exists $args->{updatedoc} ) {
+        $updatedoc = $args->{updatedoc};
+    }
+    else {
+        $updatedoc = {};
+    }
 
-  if (! exists $args->{type}) {
-      # Set JSON as the default attachment mime type
-      $args->{type}="text/json";
-  }
-  my $filename=$args->{filename};
-  if (!$uid || !$filename || !($args->{content})) {
-      croak "Missing UID, filename, or content for put_attachment\n";
-  }
+    if ( !exists $args->{type} ) {
 
-  $res = $self->head("/".$self->database."/$uid",{}, {deserializer => 'application/json'});
-  if ($res->code == 200) {
-      $revision=$res->response->header("etag");
-      $revision =~ s/^\"|\"$//g
-  } elsif ($res->code == 404) {
-  }
-  else {
-      warn "put_attachment($uid) HEAD return code: ".$res->code."\n"; 
-      return;
-  }
-  $self->clear_headers;
-  $self->set_header('If-Match' => $revision) if $revision;
-  $self->type($args->{type});
-  $res = $self->put("/".$self->database."/$uid/$filename",$args->{content}, {deserializer => 'application/json'});
-  if ($res->code != 201) {
-      warn "put_attachment($uid) PUT return code: ".$res->code."\n"; 
-  } else {
-      $self->update_basic($uid,$updatedoc);
-  }
-  return $res->code;
+        # Set JSON as the default attachment mime type
+        $args->{type} = "text/json";
+    }
+    my $filename = $args->{filename};
+    if ( !$uid || !$filename || !( $args->{content} ) ) {
+        croak "Missing UID, filename, or content for put_attachment\n";
+    }
+
+    $res = $self->head( "/" . $self->database . "/$uid",
+        {}, { deserializer => 'application/json' } );
+    if ( $res->code == 200 ) {
+        $revision = $res->response->header("etag");
+        $revision =~ s/^\"|\"$//g;
+    }
+    elsif ( $res->code == 404 ) {
+    }
+    else {
+        warn "put_attachment($uid) HEAD return code: " . $res->code . "\n";
+        return;
+    }
+    $self->clear_headers;
+    $self->set_header( 'If-Match' => $revision ) if $revision;
+    $self->type( $args->{type} );
+    $res = $self->put( "/" . $self->database . "/$uid/$filename",
+        $args->{content}, { deserializer => 'application/json' } );
+    if ( $res->code != 201 ) {
+        warn "put_attachment($uid) PUT return code: " . $res->code . "\n";
+    }
+    else {
+        $self->update_basic( $uid, $updatedoc );
+    }
+    return $res->code;
 }
 
 sub get_configdocs {
-  my ($self) = @_;
-  my ($res, $code, $data);
+    my ($self) = @_;
+    my ( $res, $code, $data );
 
-  $res = $self->get("/".$self->database."/_design/tdr/_view/configs?reduce=false&include_docs=true", {}, {deserializer => 'application/json'});
+    $res = $self->get(
+        "/"
+          . $self->database
+          . "/_design/tdr/_view/configs?reduce=false&include_docs=true",
+        {},
+        { deserializer => 'application/json' }
+    );
 
-  if ($res->code != 200) {
-      warn "_view/configs GET return code: " . $res->code . "\n";
-  }
-  if ($res->data && $res->data->{rows}) {
-      my $configdocs = {};
-      foreach my $config (@{$res->data->{rows}}) {
-          $configdocs->{$config->{'id'}}=$config->{'doc'};
-      }
-      return $configdocs;
-  }
+    if ( $res->code != 200 ) {
+        warn "_view/configs GET return code: " . $res->code . "\n";
+    }
+    if ( $res->data && $res->data->{rows} ) {
+        my $configdocs = {};
+        foreach my $config ( @{ $res->data->{rows} } ) {
+            $configdocs->{ $config->{'id'} } = $config->{'doc'};
+        }
+        return $configdocs;
+    }
 }
 
 sub get_filesystem {
-  my ($self,$docs) = @_;
-  my ($res, $code, $data);
+    my ( $self, $docs ) = @_;
+    my ( $res, $code, $data );
 
-  $res = $self->get("/".$self->database."/_design/tdr/_view/filesystem?reduce=false&include_docs=".($docs?"true":"false"), {}, {deserializer => 'application/json'});
+    $res = $self->get(
+        "/"
+          . $self->database
+          . "/_design/tdr/_view/filesystem?reduce=false&include_docs="
+          . ( $docs ? "true" : "false" ),
+        {},
+        { deserializer => 'application/json' }
+    );
 
-  if ($res->code != 200) {
-      warn "_view/configs GET return code: " . $res->code . "\n";
-  }
-  if ($res->data && $res->data->{rows}) {
-      return $res->data->{rows};
-  }
+    if ( $res->code != 200 ) {
+        warn "_view/configs GET return code: " . $res->code . "\n";
+    }
+    if ( $res->data && $res->data->{rows} ) {
+        return $res->data->{rows};
+    }
 }
 
 sub get_aip {
-    my ($self, $uid) = @_;
+    my ( $self, $uid ) = @_;
 
     $self->type("application/json");
-    my $res = $self->get("/".$self->{database}."/$uid",{}, {deserializer => 'application/json'});
-    if ($res->code == 200) {
+    my $res = $self->get( "/" . $self->{database} . "/$uid",
+        {}, { deserializer => 'application/json' } );
+    if ( $res->code == 200 ) {
         return $res->data;
     }
-    elsif ($res->code == 404) {
+    elsif ( $res->code == 404 ) {
         return;
     }
     else {
-        warn "get_aip return code: ".$res->code."\n"; 
+        warn "get_aip return code: " . $res->code . "\n";
         return;
     }
 }
 
 sub get_attachment {
-    my ($self, $uid, $attachment) = @_;
+    my ( $self, $uid, $attachment ) = @_;
 
     $self->type("application/json");
-    my $res = $self->get("/".$self->{database}."/$uid/$attachment",{}, {deserializer => 'application/json'});
-    if ($res->code == 200) {
+    my $res = $self->get( "/" . $self->{database} . "/$uid/$attachment",
+        {}, { deserializer => 'application/json' } );
+    if ( $res->code == 200 ) {
         return $res->response->content;
     }
     else {
-        warn "get_aip return code: ".$res->code."\n"; 
+        warn "get_aip return code: " . $res->code . "\n";
         return;
     }
 }
-
 
 1;
